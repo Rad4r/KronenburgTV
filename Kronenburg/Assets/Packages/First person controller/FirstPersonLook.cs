34cdd763a7a496c1a7abc6ffb99c6212586ab708
V <hanimohammad00@gmail.com>
2021-12-09 16:16:23 +0400

    Disabled camera and other optimizations
    Also movement script change

--- Kronenburg/Assets/Packages/First person controller/FirstPersonLook.cs
+++ Kronenburg/Assets/Packages/First person controller/FirstPersonLook.cs
@@ -4,15 +4,12 @@ public class FirstPersonLook : MonoBehaviour
 {
     [SerializeField]
     Transform character;
-    Vector2 currentMouseLook;
-    Vector2 appliedMouseDelta;
     public float sensitivity = 1;
-    public float smoothing = 2;
 
 
     void Reset()
     {
-        character = GetComponentInParent<FirstPersonMovement>().transform;
+        character = transform.parent;
     }
 
     void Start()
@@ -23,14 +20,9 @@ public class FirstPersonLook : MonoBehaviour
 
     void Update()
     {
-        // Get smooth mouse look.
-        Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
-        appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
-        currentMouseLook += appliedMouseDelta;
-        currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
-
-        // Rotate camera and controller.
-        transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+        Vector3 currentXRotation = character.rotation.eulerAngles + new Vector3(0, Input.GetAxis("Horizontal") * sensitivity * Time.deltaTime, 0);
+        Vector3 currentYRotation = transform.rotation.eulerAngles + new Vector3(-Input.GetAxis("Vertical") * sensitivity/2, Input.GetAxis("Horizontal") * sensitivity, 0) * Time.deltaTime;
+        character.rotation = Quaternion.Euler(currentXRotation);
+        transform.rotation = Quaternion.Euler(currentYRotation);
     }
 }
