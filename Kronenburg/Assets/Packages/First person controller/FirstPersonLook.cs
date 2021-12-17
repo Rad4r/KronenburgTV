d8171aa85b36db5b4254486510b826b1a5960081
V <hanimohammad00@gmail.com>
2021-12-17 18:54:57 +0400

    PC controls added
    By default you can move around with WASD and look with mouse. (Not important but you can enable the Apple TV version of the controls in the FirstPerson object by ticking Apple Debug)

--- Kronenburg/Assets/Packages/First person controller/FirstPersonLook.cs
+++ Kronenburg/Assets/Packages/First person controller/FirstPersonLook.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class FirstPersonLook : MonoBehaviour
 {
+    private bool appleDebug;
     [SerializeField]
     Transform character;
     public float sensitivity = 1;
+    
+    //new
+    Vector2 currentMouseLook;
+    Vector2 appliedMouseDelta;
+    public float smoothing = 2;
 
+
+    private void Awake()
+    {
+        appleDebug = FindObjectOfType<FirstPersonMovement>().AppleControlsDebug;
+
+        if (appleDebug)
+            sensitivity = 100;
+    }
 
     void Reset()
     {
@@ -20,9 +35,25 @@ public class FirstPersonLook : MonoBehaviour
 
     void Update()
     {
-        Vector3 currentXRotation = character.rotation.eulerAngles + new Vector3(0, Input.GetAxis("Horizontal") * sensitivity * Time.deltaTime, 0);
-        Vector3 currentYRotation = transform.rotation.eulerAngles + new Vector3(-Input.GetAxis("Vertical") * sensitivity/2, Input.GetAxis("Horizontal") * sensitivity, 0) * Time.deltaTime;
-        character.rotation = Quaternion.Euler(currentXRotation);
-        transform.rotation = Quaternion.Euler(currentYRotation);
+        if (appleDebug)
+        {
+            Vector3 currentXRotation = character.rotation.eulerAngles + new Vector3(0, Input.GetAxis("Horizontal") * sensitivity * Time.deltaTime, 0);
+            Vector3 currentYRotation = transform.rotation.eulerAngles + new Vector3(-Input.GetAxis("Vertical") * sensitivity/2, Input.GetAxis("Horizontal") * sensitivity, 0) * Time.deltaTime;
+            character.rotation = Quaternion.Euler(currentXRotation);
+            transform.rotation = Quaternion.Euler(currentYRotation);
+        }
+        else
+        {
+            // Get smooth mouse look.
+            Vector2 smoothMouseDelta = Vector2.Scale(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")), Vector2.one * sensitivity * smoothing);
+            appliedMouseDelta = Vector2.Lerp(appliedMouseDelta, smoothMouseDelta, 1 / smoothing);
+            currentMouseLook += appliedMouseDelta;
+            currentMouseLook.y = Mathf.Clamp(currentMouseLook.y, -90, 90);
+
+            // Rotate camera and controller.
+            transform.localRotation = Quaternion.AngleAxis(-currentMouseLook.y, Vector3.right);
+            character.localRotation = Quaternion.AngleAxis(currentMouseLook.x, Vector3.up);
+        }
+        
     }
 }
--- Kronenburg/Assets/Packages/First person controller/FirstPersonMovement.cs
+++ Kronenburg/Assets/Packages/First person controller/FirstPersonMovement.cs
@@ -2,12 +2,25 @@
 
 public class FirstPersonMovement : MonoBehaviour
 {
+    public bool AppleControlsDebug;
     public float speed = 5;
     Vector2 velocity;
 
     private void Update()
     {
-        if (Input.GetButton("Walk"))
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        if (AppleControlsDebug)
+        {
+            if (Input.GetButton("Walk"))
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+            
+        else
+        {
+            velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            transform.Translate(velocity.x, 0, velocity.y);
+        }
+        
+            
     }
 }
